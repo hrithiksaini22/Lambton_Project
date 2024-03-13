fd9daa4cbcb89ff3fe3db11ad978810e35a8e814
hrithik <122617770+hrithiksaini22@users.noreply.github.com>
2024-03-13 02:05:14 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,24 @@
 def buildImage() {
     echo "building the docker image and pushing it to our repository"
+    def imageTag = 'hrtithik9876789/app:1.0'  // Define the image tag
+
+    // Check if the image already exists in the Docker Hub repository
+    def imageExists = sh(script: "docker pull $imageTag", returnStatus: true) == 0
+
+    // If the image exists, skip the build process
+    if (imageExists) {
+        echo "Image $imageTag already exists in the repository. Skipping build."
+        return
+    }
+
+    // If the image does not exist, proceed with the build process
     withCredentials([usernamePassword(credentialsId: 'Dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t hrtithik9876789/app:1.0 .'
         sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-        sh 'docker push hrtithik9876789/app:1.1'
+        sh 'docker push hrtithik9876789/app:1.0'
     }
-} 
+}
+
 
 def deployApp() {
     echo 'deploying the application...'
